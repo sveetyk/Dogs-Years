c2ac8b92783b8b34214d0e698810107768bef3ae
sveetyk <rotari.svetlana@gmail.com>
2021-06-24 00:57:50 +0300

    Dogs Years

---
+++ dog.js
@@ -0,0 +1,21 @@
+//Age variable
+const myAge = 30;
+
+//the change variable
+var earlyYears = 2;
+
+earlyYears = earlyYears * 10.5;
+
+//Multiply the laterYears var
+var laterYears = (myAge - 2) * 4;
+
+//Store the earlyYears and laterYears var together
+var myAgeInDogYears = earlyYears + laterYears
+
+//The string method
+var myName = 'Marry';
+myName.toLowerCase();
+
+
+console.log('My name is ' + myName + ' I am ' + myAge + ' years old in human years which is ' + myAgeInDogYears + ' years old in dog years.')
+
